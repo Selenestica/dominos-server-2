c64a89852ed6a2ed188f10dda28e1d76c33bd9b3
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-03-18 22:20:09 -0500

    deploy attempt number 2

---
+++ routes/order.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const pizzaapi = require('dominos')
+const router = express.Router()
+const cors = require('cors')
+const util = require('util')
+router.use(express.urlencoded({extended: false}))
+router.use(cors())
+router.use(express.json())
+
+router.post('/add-order', async (req, res) => {
+
+    //console.log(req.body)
+    let newCustomer = new pizzaapi.Customer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address: {
+            Street: req.body.street,
+            City: req.body.city,
+            Region: req.body.state,
+            PostalCode: req.body.zip
+        },
+        email: req.body.email, 
+        phone: req.body.phone
+    })
+    
+    let chosenDeliveryMethod = req.body.chosenDeliveryMethod
+
+    //stores your store id in a variable
+    let myStore = new pizzaapi.Store({ID: req.body.chosenStore})
+    myStore.ID = req.body.chosenStore
+
+    let order = new pizzaapi.Order({
+        customer: newCustomer,
+        storeID: myStore.ID,
+        deliveryMethod: chosenDeliveryMethod
+    })
+    order.StoreID = myStore.ID
+
+    let itemArray = req.body.itemCode
+    for (item = 0; item < itemArray.length; item ++) {
+        order.addItem(
+            new pizzaapi.Item({
+                code: itemArray[item],
+                options: [],
+                quantity: 1
+            })
+        )
+    }
+
+    order.StoreOrderID = order.StoreID
+
+    //checks to see if the order will go through
+    order.validate(
+        function(result) {
+            console.log("*********************************** Order Validated ************************************************************************************************************")
+            console.log(util.inspect(result, false, null, true))
+        }
+    )
+
+    //see the price of the order without placing it
+    order.price(
+        function(result) {
+            console.log("*********************************** Price ************************************")
+            console.log(util.inspect(result, false, null, true))
+        }
+    )
+
+    //pass in cc info
+    let cardNumber = req.body.cardNumber
+    let cardInfo = new order.PaymentObject()
+        cardInfo.Amount = order.Amounts.Customer
+        cardInfo.Number = cardNumber
+        cardInfo.CardType = order.validateCC(cardNumber)
+        cardInfo.Expiration = req.body.expiration
+        cardInfo.SecurityCode = req.body.securityCode
+        cardInfo.PostalCode = req.body.cardZip
+
+    order.Payments.push(cardInfo)
+
+    //places the order
+    order.place(
+        function(result) {
+            console.log("*********************************** Order Placed! ************************************")
+            console.log(util.inspect(result, false, null, true))
+        }
+    )
+
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT
+
+// order route
+const orderRouter = require('./routes/order')
+app.use('/order', orderRouter)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+dotenv.config()
+
+app.get('/', (req, res) => {
+    res.json('/')
+})
+
+if (port == null || port == "") {
+    port = 8000;
+  }
+  app.listen(port);
